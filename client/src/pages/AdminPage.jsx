4e64d1ac2d716f86666129d7c14d5b2e1214fa99
Viktoriia Aksonova <viktoriia.aksonova@nure.ua>
2025-03-05 16:17:37 +0200

    maket

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState }from 'react'
 
 function AdminPage() {
+  const [selectedText, setSelectedText] = useState("")
+  
   return (
-    <h1>Hello admins</h1>
+    <div className="flex flex-col items-center w-full m-10">
+      <h1 className=''>Hello admins</h1>
+      <div className='flex space-x-6 m-10'>
+        <button onClick={() => setSelectedText("Users")}>Users</button>
+        <button onClick={() => setSelectedText("News")}>News</button>
+        <button onClick={() => setSelectedText("Comments")}>Comments</button>
+        <button onClick={() => setSelectedText("Reports")}>Reports</button>
+      </div>
+      <input type="search" placeholder="Пошук..." />
+      <h1>{selectedText}</h1>
+    </div>
   )
 }
 
