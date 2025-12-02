94947c038d54219608f7a1039ac702d9fd5357bb
irynazuiko-ux <iryna.zuiko@nure.ua>
2025-12-02 13:00:18 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const https = require("https");
 const fs=require("fs");
 
 const { initNewsSocket } = require("./sockets/newsSocket");
-const { startGrpcServer } = require("./config/grpc/grpcNewsServer");
 
 const usersRouter = require("./routes/users");
 const newsRouter = require("./routes/news");
 const commentsRouter = require("./routes/comments");
+const likesRouter = require("./routes/likes")
 const loginRouter = require("./routes/login");
 const registerRouter = require("./routes/register");
 
@@ -25,18 +25,17 @@ app.use(cors(corsOptions));
 app.use("/users", usersRouter);
 app.use("/news", newsRouter);
 app.use("/comments", commentsRouter);
+app.use("/likes", likesRouter)
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 
 const PORT = 8080;
 const server = https.createServer({
-  key: fs.readFileSync("./config/ssl/localhost-key.pem"),
-  cert: fs.readFileSync("./config/ssl/localhost.pem")
+  key: fs.readFileSync("localhost-key.pem"),
+  cert: fs.readFileSync("localhost.pem")
 }, app);
 
 const ws = initNewsSocket(server);
 app.locals.broadcastNewComment = ws.broadcastNewComment;
-
-startGrpcServer();
 
 server.listen(PORT, () => console.log(`HTTPS + WebSocket сервер на ${PORT}`));
